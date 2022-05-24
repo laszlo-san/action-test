c518972b5b1d749758e7815cce10b1db4788ab74
11998934+laszlo-san <11998934+laszlo-san@users.noreply.github.com>
2022-05-24 10:45:53 +0200

    feat: new feature!

--- tests/hello/index.js
+++ tests/hello/index.js
@@ -1,7 +1,15 @@
 const request = require('supertest');
 
-it('should return hello world', async () => {
+it('should return 200 statuscode', async () => {
   await request(strapi.server.httpServer)
     .get('/api/hello')
     .expect(200) // Expect response http code 200
+});
+
+
+it('should return hello world', async () => {
+  const res = await request(strapi.server.httpServer)
+    .get('/api/hello');
+
+    expect(res.text).toEqual("Hello World!");
 });
